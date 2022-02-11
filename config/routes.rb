0648318bddc9b2887f5f25a8cd102860e76f5a35
prbln <Prabhleensaini14@gamil.com>
2022-02-11 10:49:08 +0530

    working on authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :profiles
+  root to: "home#index"
+  get "/profiles/sign_out" to: 
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+  # resources :users, only: [:index]
+  
 end
